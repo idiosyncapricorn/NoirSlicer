992365d76632bd9602d819d95c7824db1adced95
idiosyncapricorn <cmorrisongavin@gmail.com>
2025-06-29 17:23:55 -0700

    Update sizing_scale.pyx

--- sizing_scale.pyx
+++ sizing_scale.pyx
@@ -1,45 +1,49 @@
 # sizing_scale.pyx
 # cython: boundscheck=False, wraparound=False, language_level=3
 
-from cython.parallel cimport prange
-cimport numpy as np
 import numpy as np
+cimport numpy as np
 ctypedef np.double_t DTYPE_t
 
-cpdef dict analyze_scale(np.ndarray[DTYPE_t, ndim=1] data, double target_min, double target_max):
+cpdef dict analyze_scale(np.ndarray[DTYPE_t, ndim=1] data,
+                         double scale_pct,
+                         double target_min,
+                         double target_max):
     """
-    Returns {
-      'min': float, 'max': float, 'mean': float, 'std': float,
-      'linear_scale': lambda x: …,
-      'zscore_scale': lambda x: …,
-      'log_scale': lambda x: …,
-      'suggested_factors': [float,…]
-    }
+    data            : raw sizes
+    scale_pct       : user‐chosen scaling in percent (e.g. 150 → 1.5×)
+    target_min,max  : desired output range
     """
     cdef Py_ssize_t n = data.shape[0]
-    cdef double smin = data[0], smax = data[0], ssum = 0.0, ssum2 = 0.0
+    cdef double factor = scale_pct / 100.0
+
+    # pre‐scale data copy
+    cdef np.ndarray[DTYPE_t, ndim=1] scaled = data * factor
+
+    # compute stats on scaled[]
+    cdef double smin = scaled[0], smax = scaled[0], ssum = 0.0, ssum2 = 0.0
     cdef Py_ssize_t i
-    # 1. one-pass stats
     for i in range(n):
-        ssum  += data[i]
-        ssum2 += data[i]*data[i]
-        if data[i] < smin: smin = data[i]
-        elif data[i] > smax: smax = data[i]
+        ssum  += scaled[i]
+        ssum2 += scaled[i]*scaled[i]
+        if scaled[i] < smin: smin = scaled[i]
+        elif scaled[i] > smax: smax = scaled[i]
     cdef double mean = ssum / n
     cdef double var  = ssum2/n - mean*mean
     cdef double std  = var > 0.0 and var**0.5 or 0.0
 
-    # 2. linear factors to map [smin,smax]→[target_min,target_max]
+    # linear map to [target_min, target_max]
     cdef double scale = (target_max - target_min) / (smax - smin)
     cdef double shift = target_min - smin * scale
 
-    # 3. collect a few “suggested_factors” around that scale
+    # suggestions around that map
     cdef list factors = [scale * f for f in (0.5, 1.0, 1.5, 2.0)]
 
     return {
+      'user_factor': factor,
       'min': smin, 'max': smax, 'mean': mean, 'std': std,
-      'linear': (scale, shift),
+      'linear_map': (scale, shift),
       'zscore': (mean, std),
-      'log': None if smin<=0 else True,
+      'log_ok': smin > 0,
       'suggested_factors': factors
     }
